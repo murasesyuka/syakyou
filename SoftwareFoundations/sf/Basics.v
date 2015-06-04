c163ba6d4a70d5d6e39b6c02a73d418a265e0af2
murase_syuka <murase.syuka+github@gmail.com>
2015-06-05 07:49:35 +0900

    Qed : nandb full pass

--- SoftwareFoundations/sf/Basics.v
+++ SoftwareFoundations/sf/Basics.v
@@ -242,11 +242,11 @@ Definition nandb (b1:bool) (b2:bool) : bool :=
 Example test_nandb1:               (nandb true false) = true.
 Proof. simpl. reflexivity. Qed.
 Example test_nandb2:               (nandb false false) = true.
-(* FILL IN HERE *) Admitted.
+Proof. simpl. reflexivity. Qed.
 Example test_nandb3:               (nandb false true) = true.
-(* FILL IN HERE *) Admitted.
+Proof. simpl. reflexivity. Qed.
 Example test_nandb4:               (nandb true true) = false.
-(* FILL IN HERE *) Admitted.
+Proof. simpl. reflexivity. Qed.
 (** [] *)
 
 (** **** Exercise: 1 star (andb3) *)
