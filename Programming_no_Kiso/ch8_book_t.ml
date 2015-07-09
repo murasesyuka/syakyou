e6745874376ed945e894eb83002eb212e8a29b03
murase_syuka <murase.syuka+github@gmail.com>
2015-07-10 07:47:09 +0900

    exer 8.2

--- Programming_no_Kiso/ch8_book_t.ml
+++ Programming_no_Kiso/ch8_book_t.ml
@@ -9,6 +9,19 @@ type book_t = {
 let book1 = {title = "foo"; author = "i"; publisher = "you"; price = 42; isbn = 1234567890 }
 let book2 = {title = "bar"; author = "my"; publisher = "your"; price = 72; isbn = 1234567890 }
 let book3 = {title = "baz"; author = "me"; publisher = "you"; price = 108; isbn = 1234567890 }
+
+
+type okozukai_t = {
+    namae : string;
+    nedan : int;
+    basyo : string;
+    data  : string;
+  }
+
+let kozukai1 = { namae = "hoge"; nedan = 42; basyo = "kyouto"; data = "2015-07-10" }
+let kozukai2 = { namae = "homu"; nedan = 72; basyo = "oosaka"; data = "2015-07-11" }
+let kozukai3 = { namae = "fuga"; nedan = 21; basyo = "hyougo"; data = "2015-07-12" }		 
+
 	      
 (* purpose : todo *)
 (* todo : *)
