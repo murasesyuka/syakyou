005602375c35e9b5f3a68b59dfd4169332823998
murase_syuka <murase.syuka+github@gmail.com>
2015-07-10 00:35:05 +0900

    add : add pair func

--- Programming_no_Kiso/ch7_seiseki.ml
+++ Programming_no_Kiso/ch7_seiseki.ml
@@ -13,3 +13,12 @@ let test2 = seiseki ("bar",76) = "bar良"
 let test3 = seiseki ("baz",61) = "baz可"
 let test4 = seiseki ("hoge",60) = "hoge不可"
 				   
+(* purpose : add *)
+(* add : int * int -> int *)
+let add pair = 0
+
+(* test *)
+let test_add1 = add (0, 0) = 0
+let test_add2 = add (3, 5) = 8
+let test_add3 = add (3, -5) = -2
+
