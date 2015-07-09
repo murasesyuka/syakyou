5ecddabdd06bddc6078a1d5cc55776e1cdc43177
murase_syuka <murase.syuka+github@gmail.com>
2015-07-10 07:43:13 +0900

    exer 8.1

---
+++ Programming_no_Kiso/ch8_book_t.ml
@@ -0,0 +1,16 @@
+type book_t = {
+    title : string;
+    author: string;
+    publisher : string;
+    price : int;
+    isbn  : int;
+  }
+
+let book1 = {title = "foo"; author = "i"; publisher = "you"; price = 42; isbn = 1234567890 }
+let book2 = {title = "bar"; author = "my"; publisher = "your"; price = 72; isbn = 1234567890 }
+let book3 = {title = "baz"; author = "me"; publisher = "you"; price = 108; isbn = 1234567890 }
+	      
+(* purpose : todo *)
+(* todo : *)
+
+(* test *)
