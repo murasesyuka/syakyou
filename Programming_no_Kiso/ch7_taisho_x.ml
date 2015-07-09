4f882eb37ce3be51dfc2b6253ed71b0bf3767482
murase_syuka <murase.syuka+github@gmail.com>
2015-07-10 01:02:02 +0900

    align

--- Programming_no_Kiso/ch7_taisho_x.ml
+++ Programming_no_Kiso/ch7_taisho_x.ml
@@ -11,9 +11,11 @@ let test3 = taisho_x (4, -2) = (-4, -2)
 
 (* purpose : chuten *)
 (* chuten : int * int -> int * int -> int *)
-let chuten start_p end_p  = match start_p with
-    (x1, y1) -> match end_p with
-		  (x2, y2) -> ((x1+x2)/2, (y1+y2)/2)
+let chuten start_p end_p  = 
+  match start_p with
+    (x1, y1) -> 
+    match end_p with
+      (x2, y2) -> ((x1+x2)/2, (y1+y2)/2)
 
 (* test *)
 let test1 = chuten (0, 0) (0, 0)  = (0, 0)
