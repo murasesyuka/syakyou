7f4e6b8cf8e231caba4cc1a9dc7d33772d008ee7
murase_syuka <murase.syuka+github@gmail.com>
2015-08-10 13:49:09 +0900

    add : skelton and test test

---
+++ Programming_no_Kiso/ch10_add_to_each.ml
@@ -0,0 +1,17 @@
+(* purpose : add to each *)
+(* add_to_each : int -> int list list -> int list list *)
+let rec add_to_each n lst = match lst with
+    [] -> []
+  | x::xs -> []
+
+(* test *)
+let test1 = add_to_each 1 [] = []
+let test2 = add_to_each 1 [2] = [1;2]
+let test3 = add_to_each 1 [[2];[2;3]] = [[1;2];[1;2;3]]
+let test3 = add_to_each 1 [[2];[2;3];[2;3;4]] = [[1;2];[1;2;3];[1;2;3;4]]
+
+let test5 = prefix [] = []
+let test6 = prefix [1] = [[1]]
+let test7 = prefix [1;2] = [[1];[1;2]]
+let test8 = prefix [1;2;3;4] = [[1];[1;2];[1;2;3];[1;2;3;4]]
+
