0332b1d559f2ae679d9db7fbb82e1089cf3aee8a
murase_syuka <murase.syuka+github@gmail.com>
2015-08-10 13:58:27 +0900

    ok : add_to_each()

--- Programming_no_Kiso/ch10_add_to_each.ml
+++ Programming_no_Kiso/ch10_add_to_each.ml
@@ -2,11 +2,15 @@
 (* add_to_each : int -> int list list -> int list list *)
 let rec add_to_each n lst = match lst with
     [] -> []
-  | x::xs -> []
+  | x::xs -> (n :: x) :: add_to_each n xs
 
+let rec prefix lst = match lst with
+    [] -> []
+  | x::xs -> []
+				     
 (* test *)
 let test1 = add_to_each 1 [] = []
-let test2 = add_to_each 1 [2] = [1;2]
+let test2 = add_to_each 1 [[2]] = [[1;2]]
 let test3 = add_to_each 1 [[2];[2;3]] = [[1;2];[1;2;3]]
 let test3 = add_to_each 1 [[2];[2;3];[2;3;4]] = [[1;2];[1;2;3];[1;2;3;4]]
 
