64f0acfa0e5114cf27b23ae1c60e9158e4c78be2
murase_syuka <murase.syuka+github@gmail.com>
2015-08-12 00:37:54 +0900

    exer 10.5

--- Programming_no_Kiso/ch10_minimum.ml
+++ Programming_no_Kiso/ch10_minimum.ml
@@ -14,3 +14,30 @@ let test3 = minimum [3;2] = 2
 let test4 = minimum [3;2;6;4;1;8] = 1
 
 
+(* datatype : 学生一人の成績データ *)
+type gakusei_t = {
+    namae : string;
+    tensuu : int;
+    seiseki : string;
+  }
+
+(* purpose : gakusei max *)
+(* gakusei_max : gakusei_t list -> gakusei_t *)
+let rec gakusei_max lst = match lst with
+    [] -> {namae = ""; tensuu = 0; seiseki=""}
+  | x::xs -> let rest = gakusei_max xs in
+     if x.tensuu > rest.tensuu
+     then x
+     else rest
+
+
+let test5 = gakusei_max [{namae = "foo"; tensuu = 80; seiseki = ""};
+			 {namae = "bar"; tensuu = 70; seiseki = ""};
+			 {namae = "baz"; tensuu = 60; seiseki = ""};
+			 {namae = "hoo"; tensuu = 50; seiseki = ""}] = {namae = "foo"; tensuu = 80; seiseki = ""}
+let test6 = gakusei_max [{namae = "foo"; tensuu = 80; seiseki = ""};
+			 {namae = "bar"; tensuu = 70; seiseki = ""};
+			 {namae = "baz"; tensuu = 60; seiseki = ""};
+			 {namae = "hoo"; tensuu = 90; seiseki = ""}] = {namae = "hoo"; tensuu = 90; seiseki = ""}
+
+									 
