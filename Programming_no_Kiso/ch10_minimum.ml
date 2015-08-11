84a1268788fcc094b3aeff52445c6a757666724e
murase_syuka <murase.syuka+github@gmail.com>
2015-08-12 08:17:33 +0900

    add : shukei()...

--- Programming_no_Kiso/ch10_minimum.ml
+++ Programming_no_Kiso/ch10_minimum.ml
@@ -47,12 +47,11 @@ let test6 = gakusei_max [{namae = "foo"; tensuu = 80; seiseki = ""};
 let rec shukei lst = match lst with
     [] -> (0, 0, 0, 0)
   | {namae = n; tensuu = t; seiseki=s} :: rest ->
-     let shukei_rest = shukei rest in
-     match shukei_rest with
-     (a,b,c,d) ->  if s = "A"      then (a+1, b, c, d)
-		   else if s = "B" then (a, b+1, c, d)
-		   else if s = "C" then (a, b, c+1, d)
-		   else (a, b, c, d+1)
+     let (a,b,c,d) = shukei rest in
+     if s = "A"      then (a+1, b, c, d)
+     else if s = "B" then (a, b+1, c, d)
+     else if s = "C" then (a, b, c+1, d)
+     else (a, b, c, d+1)
 
 (* test *)
 let test7 = shukei [{namae = "foo"; tensuu = 80; seiseki = "A"};
