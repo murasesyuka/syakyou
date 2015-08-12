06b46e3503165d3a5094160f3402435e6c073603
murase_syuka <murase.syuka+github@gmail.com>
2015-08-12 21:49:47 +0900

    add : merge()...ok

--- Programming_no_Kiso/ch10_append.ml
+++ Programming_no_Kiso/ch10_append.ml
@@ -17,13 +17,14 @@ let rec merge lst1 lst2 = match (lst1,lst2) with
     ([], []) -> []
   | (_, []) -> lst1
   | ([], _) -> lst2
-  | (x::xs, y::ys) -> []
+  | (x::xs, y::ys) -> 
+     if x<y then x :: merge xs lst2
+     else y :: merge lst1 ys
 
 (* test *)
 let test6 = merge [] [] = []
 let test7 = merge [] [1;2] = [1;2]
 let test8 = merge [1;2] [] = [1;2]
-let test9 = merge [1;2] [3;4] = [1;2;3;4]
-let test0 = merge ["a";"b";"c"] ["d";"e"] = ["a";"b";"c";"d";"e"]
-
-
+let test9 = merge [1;3] [2;4] = [1;2;3;4]
+let test0 = merge [1;4] [2;3] = [1;2;3;4]
+let test01 = merge [1;4] [1;3] = [1;1;3;4]
