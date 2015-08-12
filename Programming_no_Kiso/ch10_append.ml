118bcb45b832d2959710d7fd13ac277b5561a5e6
murase_syuka <murase.syuka+github@gmail.com>
2015-08-12 21:28:03 +0900

    add : append()

---
+++ Programming_no_Kiso/ch10_append.ml
@@ -0,0 +1,12 @@
+(* purpose : *)
+(* append : 'a list -> 'a list -> 'a list *)
+let rec append lst1 lst2 = match lst1 with
+    [] -> lst2
+  | x::xs -> x :: append xs lst2
+
+(* test *)
+let test1 = append [] [] = []
+let test2 = append [] [1;2] = [1;2]
+let test3 = append [1;2] [] = [1;2]
+let test4 = append [1;2] [3;4] = [1;2;3;4]
+let test5 = append ["a";"b";"c"] ["d";"e"] = ["a";"b";"c";"d";"e"]
