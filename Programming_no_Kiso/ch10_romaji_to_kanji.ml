5ae045c01220fdd3b3bf408d51d0906e309c8c4c
murase_syuka <murase.syuka+github@gmail.com>
2015-08-13 10:24:36 +0900

    exer 10.10. but test fail for character encoding

---
+++ Programming_no_Kiso/ch10_romaji_to_kanji.ml
@@ -0,0 +1,14 @@
+(* purpose :  *)
+(* romaji_to_kanji : string -> elimei_t list -> string *)
+let rec romaji_to_kanji romaji lst = match lst with
+    [] -> "なし"
+  | {kanji=kanj; kana=kana; romaji=roma; shozoku=shoz}::rest -> 
+     if roma = romaji
+     then kanj
+     else romaji_to_kanji romaji rest
+
+(* test *)
+let test1 = romaji_to_kanji "yoyogiuehara" global_ekimei_list = "代々木上原"
+let test2 = romaji_to_kanji "yotsuya" global_ekimei_list = "四ツ谷"
+
+
