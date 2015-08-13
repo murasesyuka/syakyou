af2c85f971e82a4201684d47cbffb9e39d1d987f
murase_syuka <murase.syuka+github@gmail.com>
2015-08-13 14:50:14 +0900

    exer 10.12

--- Programming_no_Kiso/ch10_romaji_to_kanji.ml
+++ Programming_no_Kiso/ch10_romaji_to_kanji.ml
@@ -29,7 +29,23 @@ let rec get_ekikan_kyori eki1 eki2 lst = match lst with
 
 let test3 = get_ekikan_kyori "代々木上原" "代々木公園" global_ekikan_list = 1.0
 let test4 = get_ekikan_kyori "代々木上原" "明治神宮前" global_ekikan_list = Float.infinity
-let test3 = get_ekikan_kyori "営団成増" "和光市" global_ekikan_list = 2.1
+let test5 = get_ekikan_kyori "営団成増" "和光市" global_ekikan_list = 2.1
 									      
+(* purpose :  *)
+(* kyori_wo_hyouji : string -> string -> string *)
+let kyori_wo_hyouji eki1_romaji eki2_romaji = 
+  let eki1 = romaji_to_kanji eki1_romaji global_ekimei_list in
+  let eki2 = romaji_to_kanji eki2_romaji global_ekimei_list in
+  if eki1 = ""
+  then Printf.sprintf "%s駅は存在しません" eki1_romaji
+  else if eki2 = ""
+  then Printf.sprintf "%s駅は存在しません" eki2_romaji
+  else let kyori = get_ekikan_kyori eki1 eki2 global_ekikan_list in
+       if kyori = Float.infinity
+       then Printf.sprintf "%s駅と%s駅は繋がっていません" eki1 eki2
+       else Printf.sprintf "%s駅から%s駅までは%2.1fkmです" eki1 eki2 kyori
 
-
+let test6 = kyori_wo_hyouji "foo" "yoyogiuehara" = "foo駅は存在しません"
+let test7 = kyori_wo_hyouji "yoyogiuehara" "bar"  = "bar駅は存在しません"
+let test8 = kyori_wo_hyouji "eidannarimasu" "wakousi" = "営団成増駅から和光市駅までは2.1kmです"
+let test9 = kyori_wo_hyouji "eidanakakuka" "wakousi" = "営団赤塚駅と和光市駅は繋がっていません"
