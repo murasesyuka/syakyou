227b63fb91b0aee8669960d41419045bb6336891
murase_syuka <murase.syuka+github@gmail.com>
2016-01-02 21:23:27 +0900

    add : mult_acc0

--- From_Mathematics_to_Generic_Programming/2-1_mul.cpp
+++ From_Mathematics_to_Generic_Programming/2-1_mul.cpp
@@ -25,8 +25,19 @@ int multiply_by_15(int a)
 	return (c+c) +b;
 }
 
+//2.2
+int mult_acc0(int r, int n, int a)
+{
+	if (n == 1) return r + a;
+	if (odd(n)){
+		return mult_acc0( r + a, half(n), a+a);
+	} else {
+		return mult_acc0( r, half(n), a+a);
+	}
+}
 
 int main(){
 	using namespace std;
 	cout << multiply_by_15(3) << endl;
+	cout << mult_acc0(0, 15, 3) << endl;
 }
