f31af781ac3c8aca851dcda6660b60702521a99c
murase_syuka <murase.syuka+github@gmail.com>
2016-01-02 17:20:07 +0900

    add : multi1

--- From_Mathematics_to_Generic_Programming/2-1_mul.cpp
+++ From_Mathematics_to_Generic_Programming/2-1_mul.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 
-// Egyptian multiplication
 int multiply0(int n, int a)
 {
 	if (n == 1) return a;
 	return multiply0(n-1, a) +a;
 }
 
+// Egyptian multiplication
+bool odd(int n) { return n & 0x1; }
+int half(int n) { return n >> 1; }
+
+int multiply1(int n, int a)
+{
+	if (n == 1) return a;
+	int result = multiply1(half(n), a + a);
+	if (odd(n)) result = result + a;
+	return result;
+}
+
 int main(){
 	using namespace std;
-	cout << multiply0(5,4) << endl;
+	cout << multiply1(41,59) << endl;
 }
