da73d8ab1241b6da47b1f225656a262b3ec4aae9
murase_syuka <murase.syuka+github@gmail.com>
2016-03-15 22:16:29 +0900

    do exercise 24.

--- TaPL/www.cis.upenn.edu/~bcpierce/tapl/checkers/fullomega/test.f
+++ TaPL/www.cis.upenn.edu/~bcpierce/tapl/checkers/fullomega/test.f
@@ -94,3 +94,72 @@ lambda X.
         (pair [List X] [List X] (nil [X]) (nil [X]))))
     as List X; 
 
+stackADT =
+{*List Nat,
+  {new = nil [Nat]}}
+  as {Some Stack, {new:Stack}};
+
+stack2ADT =
+      {*List Nat,
+       {new = nil [Nat],
+       isempty = isnil [Nat]}}
+       as
+       {Some Stack, 
+       {new:Stack,
+       isempty:Stack->Bool}};
+
+       
+stack3ADT =
+      {*List Nat,
+       {new = nil [Nat],
+       push = lambda n:Nat. lambda l:List Nat. cons [Nat] n l,
+       top = lambda l:List Nat. head [Nat] l,
+       pop = lambda l:List Nat. tail [Nat] l,
+       isempty = isnil [Nat]}}
+       as {Some Stack,
+       {new:Stack,
+       push:Nat->Stack->Stack,
+       top:Stack->Nat,
+       pop:Stack->Stack,
+       isempty:Stack->Bool}};
+
+let {Stack,stack} = stack3ADT in
+      stack.top (stack.push 5 (stack.push 3 stack.new));
+
+/*
+let {Stack,stack} = stack3ADT in
+      stack.pop (stack.push 7 (stack.push 4 stack.new));
+      */
+
+counter1ADT =
+      {*Nat,
+      {new = 1,
+      get = lambda i:Nat. i,
+      inc = lambda i:Nat. succ(i)}}
+      as { Some Counter,
+      {new:Counter,get:Counter->Nat,inc:Counter->Counter}};
+      
+let {Counter,cnt} = counter1ADT in
+      cnt.get (cnt.inc (cnt.inc cnt.new));
+
+      /*
+let {Counter,cnt} = counter1ADT in
+let {FF,ff} = 
+      {*Counter,
+       {new = counter.new,
+        read = lambda c:Counter. iseven (counter.get c),
+        toggle=lambda c:Counter. counter.inc c,
+        reset =lambda c:Counter. counter.new}}
+     as { Some FF,
+        {new: FF, read: FF->Bool, toggle: FF->FF, reset:FF->FF}} in
+ff.toggle (ff.toggle ff.new);
+*/
+
+counterADT =
+      {*Ref Nat,
+       {new = lambda _:Unit. ref 1,
+        get = lambda r:Ref Nat. !r,
+        inc = lambda r:Ref Nat. r := succ(!r)}}
+      as { Some Counter,
+      {new:Unit->Counter,get:Counter->Nat,inc:Counter->Unit}};
+
